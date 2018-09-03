77106d301e1ca853596239e2e65eab6798fe0949
Supran <supsng01@in.ibm.com>
2018-09-03 14:39:48 +0530

    added autocomplete

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,7 @@ import axios from 'axios';
 // https://firebase.google.com/docs/functions/typescript
 
 const _GOOGLE_TEXTSEARCH_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json";
+const _GOOGLE_AUTOSEARCH_URL = "https://maps.googleapis.com/maps/api/place/autocomplete/json";
 
 const CORS = require('cors')({origin:true});
 
@@ -13,6 +14,7 @@ export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
+/// This is for textsearch google places api
 export  const searchPlace = functions.https.onRequest( (req,res) => {
   CORS( req , res , async () => {
     try{
@@ -33,3 +35,28 @@ export  const searchPlace = functions.https.onRequest( (req,res) => {
     }
   })
 });
+
+/// This is for textsearch google autocomplete api
+export const autoCompleteSearch =  functions.https.onRequest( (req , res) => {
+  CORS( req , res , async () => {
+    try{
+      const { key , text , location , radius } = req.query;
+
+      const config = {
+        params : {
+            input:text,
+            key:key,
+            location:location,
+            radius:radius,
+            strictbounds:true
+        }
+      };
+  
+      const result = await axios.get(_GOOGLE_AUTOSEARCH_URL,config);
+      res.send(result.data);  
+    }
+    catch(error) {
+      res.send(error);
+    }
+  } );
+} );
